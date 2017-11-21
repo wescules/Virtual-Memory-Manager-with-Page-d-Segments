60fef3f255452fc5c8dd0117062070ee24b6f2df
Wesley Andrade <wafermonster@yahoo.com>
2017-11-21 14:13:29 -0600

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//LRU-X Algorithm
+//LDF Algorithm
+//WS Algorithm
+//OPT-X Algorithm
+//FIFO Algorithm
+//LIFO Algorithm
+
+
+//page fault handler function
+
+//schedules all I/O to the paging disk
+void startDisk(int process_ID, string read_write, int frame_index, string disk_addr){
+    
+}
+
+
+int main(){
+    short total_number_of_page_frames;
+    short maximum_segment_length;
+    short page_size;
+    int 
+    return 0;
+}
